fef283cc06216f768eb5710c37e62eae97b056c4
AitorSoto <aitor.soto@iesdoctorbalmis.com>
2019-11-21 09:32:37 +0100

    Agregar archivos de proyecto.

---
+++ Prueba examen/ConverterImagen.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Prueba_examen
+{
+    class ConverterImagen : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string valor = value as string;
+            switch (valor)
+            {
+                case "sol":
+                    valor = "sunny.png";
+                    break;
+                case "nubes":
+                    valor = "cloudy.png";
+                    break;
+                case "nieve":
+                    valor = "snow.png";
+                    break;
+                default:
+                    break;
+            }
+            return valor;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Prueba examen/ConvertidorMinimoTemperatura.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Prueba_examen
+{
+    class ConvertidorMinimoTemperatura : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double temperatura = (int)value;
+            temperatura = (temperatura - 32) / 1.8;
+            temperatura = (int)temperatura;
+            return temperatura;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
